a138162d22d06a7251207b51fa343d7e9881aae9
Gustavo Paganucci <contatopaganucci@gmail.com>
2025-03-06 17:50:54 -0300

    atualização do simulador

--- src/main/java/view/MainView.java
+++ src/main/java/view/MainView.java
@@ -7,20 +7,20 @@ public class MainView {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Simulador de Financiamento Imobiliario");
-        System.out.print("Digite o valor do imovel: ");
+        System.out.print(" Digite o valor do imovel: ");
         double valorImovel = scanner.nextDouble();
 
-        System.out.print("Digite o valor da entrada: ");
+        System.out.print(" Digite o valor da entrada: ");
         double entrada = scanner.nextDouble();
 
-        System.out.print("Digite o prazo (meses): ");
+        System.out.print(" Digite o prazo (meses): ");
         int prazo = scanner.nextInt();
 
-        System.out.print("Digite a taxa de juros anual (%): ");
+        System.out.print(" Digite a taxa de juros anual (%): ");
         double taxaJuros = scanner.nextDouble();
 
         double prestacao = FinanciamentoController.simularFinanciamento(valorImovel, entrada, prazo, taxaJuros);
-        System.out.printf("Digite a prestacao mensal: R$ %.2f%n", prestacao);
+        System.out.printf(" Digite a prestacao mensal: R$ %.2f%n", prestacao);
 
         scanner.close();
     }
